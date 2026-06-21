53d3b89e708117671d2f92826a458eb13aab8150
Arnavdhir007 <arnavdhir007@gmail.com>
2026-06-21 18:15:02 +0530

    Add files via upload

---
+++ fathers day.js
@@ -0,0 +1,7 @@
+function openDadPopup() {
+    document.getElementById("dadPopup").style.display = "flex";
+}
+
+function closeDadPopup() {
+    document.getElementById("dadPopup").style.display = "none";
+}
